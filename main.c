c405e9b1bda9691c38529ae09dbaf2b0f6455a68
Dalton Burke <dmtburke@gmail.com>
2018-01-21 18:34:03 -0700

    program outline

--- main.c
+++ main.c
@@ -1,7 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+struct bin_tree {
+
+};
+
+typedef struct bin_tree node;
+
+//Prototype
+void delTree(node *tree);
+void printPostorder(node *tree);
+void printInorder(node *tree);
+void printPreorder(node *tree);
+void insert(node ** tree, char* val);
+void toOutput(node * tree, FILE *fileOutput);
+node* search(node ** tree, char* val);//WIP, only searches head. Might not need function
+
+
 int main(int argc, char **argv) {
 
     exit(0);
+}
+
+
+void delTree(node *tree)
+{
+  
+}
+
+void printPostorder(node *tree)
+{
+  
+}
+
+void printInorder(node *tree)
+{
+  
+}
+
+void printPreorder(node *tree)
+{
+  
+}
+
+void insert(node ** tree, char* val)
+{
+  
+}
+void toOutput(node * tree, FILE *fileOutput)
+{
+  
+}
+
+node* search(node ** tree, char* val)//WIP, only searches head. Might not need function
+{
+  
 }
